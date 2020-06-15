ab3756309b005fe4f51027f95bbdbbf380cead1d
Eduardo Ramos <eduardorxs@gmail.com>
2020-06-14 22:54:21 -0300

    'Funções'

---
+++ biblioteca.py
@@ -0,0 +1,10 @@
+def gera_nome_convite(convite):
+    posicao_final = len(convite)
+    posicao_inicial = posicao_final - 4
+    parte1 = convite[0:4]
+    parte2 = convite[posicao_inicial: posicao_final]
+    return parte1 + ' ' + parte2
+
+
+def envia_convite(nome_formatado):
+    print 'Enviando convite para %s' % (nome_formatado)
