e6fba291adecb58cbe80c223a3bedb56c9a6bcc5
Eduardo Ramos <eduardorxs@gmail.com>
2020-06-14 23:14:01 -0300

    If, For, While

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+def cadastrar(nomes):
+    print 'Digite o nome:'
+    nome = raw_input()
+    nomes.append(nome)
+
+
+def listar(nomes):
+    print 'Listando nomes: '
+    for nome in nomes:
+        print nome
+
+
+def menu():
+    nomes = []
+    escolha = ''
+    while(escolha != '0'):
+        print 'Digite 2 para mostrar lista de nomes, 1 para cadastrar, 0 para terminar'
+        escolha = raw_input()
+
+        if(escolha == '1'):
+            cadastrar(nomes)
+
+        if(escolha == '2'):
+            listar(nomes)
+
+
+menu()
