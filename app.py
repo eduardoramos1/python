070af746b715f5aa4b594001ff43f69535c92ae4
Eduardo Ramos <eduardorxs@gmail.com>
2020-06-19 02:35:33 -0300

    Regexp e funcao filtrar

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 # -*- coding: UTF-8 -*-
+import re
+
 
 def cadastrar(nomes):
     print 'Digite o nome:'
@@ -12,10 +14,18 @@ def listar(nomes):
         print nome
 
 
+def filtrar(nomes):
+    print 'Digite o nome que deseja procurar'
+    nome = raw_input()
+    nome_filtrado = re.findall(nome, ''.join(nomes))
+    print nome_filtrado
+
+
 def menu():
     nomes = []
     escolha = ''
     while(escolha != '0'):
+        print 'Digite 3 para procurar um nome'
         print 'Digite 2 para mostrar lista de nomes, 1 para cadastrar, 0 para terminar'
         escolha = raw_input()
 
@@ -24,6 +34,9 @@ def menu():
 
         if(escolha == '2'):
             listar(nomes)
+
+        if(escolha == '3'):
+            filtrar(nomes)
 
 
 menu()
